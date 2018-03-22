d0e7bca54b0e7a030103f73bae73be0f2894acc1
pamela <pamckp@gmail.com>
2018-03-22 18:35:56 -0300

    Rotas get e getBySlug inseridas

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,7 +1,47 @@
 'use strict';
 
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product');
+
+exports.get = (req, res, next) => {
+    Product
+        .find({
+            active: true
+        }, 'title price slug')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
+
+exports.getBySlug = (req, res, next) => {
+    Product
+        .findOne({
+            slug: req.params.slug,
+            active: true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
+
 exports.post = (req, res, next) => {
-    res.status(201).send(req.body);
+    var product = new Product(req.body);
+    product
+        .save()
+        .then(x => {
+            res.status(201).send({
+                message: 'Produto cadastrado com sucesso!'
+            });
+        }).catch(e => {
+            res.status(400).send({
+            message: 'Falha ao cadastraro produto!',
+            data: e
+        });
+    });    
 };
 
 
